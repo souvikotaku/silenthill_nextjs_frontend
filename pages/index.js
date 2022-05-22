231f603d296ead9c1c9f76b0f289b16071248104
souvik herokuapi <souvikherokuapi@gmail.com>
2022-05-22 13:01:00 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -29,13 +29,13 @@ export default function Home() {
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></link>
         <script
           async
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></script>
       </Head>
       <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
